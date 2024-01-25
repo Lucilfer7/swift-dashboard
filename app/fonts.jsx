3695f909c79725602146967e25e8aa726c8a7d13
Franco Bera Platino <37113458+Lucilfer7@users.noreply.github.com>
2024-01-24 21:25:36 -0300

    first commit

---
+++ app/fonts.jsx
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Tangerine } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
+export const tangerine = Tangerine({ subsets: ["latin"], weight: ["400", "700"] });
